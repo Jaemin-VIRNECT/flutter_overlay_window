13223e2d179ff8d2259e25e7dfa44613c1b14292
Jaemin-VIRNECT <58459139+Jaemin-VIRNECT@users.noreply.github.com>
2023-12-27 13:45:42 +0900

    test 2

--- android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
+++ android/src/main/java/flutter/overlay/window/flutter_overlay_window/OverlayService.java
@@ -41,26 +41,18 @@ import io.flutter.plugin.common.BasicMessageChannel;
 import io.flutter.plugin.common.JSONMessageCodec;
 import io.flutter.plugin.common.MethodChannel;
 
-public class OverlayService extends Service implements View.OnTouchListener {
+public class OverlayService extends Service{
     private Resources mResources;
 
     public static final String INTENT_EXTRA_IS_CLOSE_WINDOW = "IsCloseWindow";
     public static boolean isRunning = false;
     private WindowManager windowManager = null;
     private FlutterView flutterView;
-    private MethodChannel flutterChannel = new MethodChannel(FlutterEngineCache.getInstance().get(OverlayConstants.CACHED_TAG).getDartExecutor(), OverlayConstants.OVERLAY_TAG);
     private BasicMessageChannel<Object> overlayMessageChannel = new BasicMessageChannel(FlutterEngineCache.getInstance().get(OverlayConstants.CACHED_TAG).getDartExecutor(), OverlayConstants.MESSENGER_TAG, JSONMessageCodec.INSTANCE);
     private int clickableFlag = WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE | WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
             WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS | WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN;
-
-    private Handler mAnimationHandler = new Handler();
-    private float lastX, lastY;
-    private int lastYPosition;
-    private boolean dragging;
     private static final float MAXIMUM_OPACITY_ALLOWED_FOR_S_AND_HIGHER = 0.8f;
     private Point szWindow = new Point();
-    private Timer mTrayAnimationTimer;
-    private TrayAnimationTimerTask mTrayTimerTask;
     private int initialWidth = 0;
     private int initialHeight = 0;
     private boolean initialOrientationIsLandscape = false;
@@ -117,17 +109,6 @@ public class OverlayService extends Service implements View.OnTouchListener {
         flutterView.setFocusableInTouchMode(true);
         flutterView.setBackgroundColor(Color.TRANSPARENT);
 
-        flutterChannel.setMethodCallHandler((call, result) -> {
-            if (call.method.equals("updateFlag")) {
-                String flag = call.argument("flag").toString();
-                updateOverlayFlag(result, flag);
-            } else if (call.method.equals("resizeOverlay")) {
-                int width = call.argument("width");
-                int height = call.argument("height");
-                resizeOverlay(width, height, result);
-            }
-        });
-
         overlayMessageChannel.setMessageHandler((message, reply) -> {
             WindowSetup.messenger.send(message);
         });
@@ -159,87 +140,51 @@ public class OverlayService extends Service implements View.OnTouchListener {
         }
         params.gravity = WindowSetup.gravity;
         flutterView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);
-        flutterView.addOnLayoutChangeListener((view, newX, newY, newWidth, newHeight, oldX, oldY, oldWidth, oldHeight) -> {
-            WindowManager windowService = (WindowManager) getSystemService(WINDOW_SERVICE);
-            int currentRotation = windowService.getDefaultDisplay().getRotation();
-            boolean isLandscape = false;
-            if (Surface.ROTATION_0 == currentRotation) {
-                isLandscape = false;
-            } else if (Surface.ROTATION_180 == currentRotation) {
-                isLandscape = false;
-            } else if (Surface.ROTATION_90 == currentRotation) {
-                isLandscape = true;
-            } else if (Surface.ROTATION_270 == currentRotation) {
-                isLandscape = true;
-            }
-
-            if (oldWidth == 0 && oldHeight == 0) {
-                initialWidth = newWidth;
-                initialHeight = newHeight;
-                initialOrientationIsLandscape = isLandscape;
-            }
-
-            mCurrentWidth = newWidth;
-            mCurrentHeight = newHeight;
-            mIsLandscape = isLandscape;
-            mCurrentRotation = currentRotation;
-
-            int width = isLandscape ? Math.max(initialHeight, initialWidth) : Math.min(
-                    initialHeight,
-                    initialWidth
-            );
-            int height = isLandscape ? Math.min(initialHeight, initialWidth) : Math.max(
-                    initialHeight,
-                    initialWidth
-            );
-
-            params.width = width;
-            params.height = height;
-            params.gravity = Gravity.LEFT | Gravity.TOP;
-            windowManager.updateViewLayout(flutterView, params);
-            String resultString = isLandscape + "|" + width + "|" + height;
-            overlayMessageChannel.setMessageHandler((message, reply) -> {
-                WindowSetup.messenger.send(resultString);
-            });
-        });
+//        flutterView.addOnLayoutChangeListener((view, newX, newY, newWidth, newHeight, oldX, oldY, oldWidth, oldHeight) -> {
+//            WindowManager windowService = (WindowManager) getSystemService(WINDOW_SERVICE);
+//            int currentRotation = windowService.getDefaultDisplay().getRotation();
+//            boolean isLandscape = false;
+//            if (Surface.ROTATION_0 == currentRotation) {
+//                isLandscape = false;
+//            } else if (Surface.ROTATION_180 == currentRotation) {
+//                isLandscape = false;
+//            } else if (Surface.ROTATION_90 == currentRotation) {
+//                isLandscape = true;
+//            } else if (Surface.ROTATION_270 == currentRotation) {
+//                isLandscape = true;
+//            }
+//
+//            if (oldWidth == 0 && oldHeight == 0) {
+//                initialWidth = newWidth;
+//                initialHeight = newHeight;
+//                initialOrientationIsLandscape = isLandscape;
+//            }
+//
+//            mCurrentWidth = newWidth;
+//            mCurrentHeight = newHeight;
+//            mIsLandscape = isLandscape;
+//            mCurrentRotation = currentRotation;
+//
+//            int width = isLandscape ? Math.max(initialHeight, initialWidth) : Math.min(
+//                    initialHeight,
+//                    initialWidth
+//            );
+//            int height = isLandscape ? Math.min(initialHeight, initialWidth) : Math.max(
+//                    initialHeight,
+//                    initialWidth
+//            );
+//
+//            params.width = width;
+//            params.height = height;
+//            params.gravity = Gravity.LEFT | Gravity.TOP;
+//            windowManager.updateViewLayout(flutterView, params);
+//            String resultString = isLandscape + "|" + width + "|" + height;
+//        });
 
         params.gravity = Gravity.LEFT | Gravity.TOP;
-        flutterView.setOnTouchListener(this);
         windowManager.addView(flutterView, params);
         return START_STICKY;
     }
-
-    private void updateOverlayFlag(MethodChannel.Result result, String flag) {
-        if (windowManager != null) {
-            WindowSetup.setFlag(flag);
-            WindowManager.LayoutParams params = (WindowManager.LayoutParams) flutterView.getLayoutParams();
-            params.flags = WindowSetup.flag | WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS |
-                    WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN |
-                    WindowManager.LayoutParams.FLAG_LAYOUT_INSET_DECOR | WindowManager.LayoutParams.FLAG_HARDWARE_ACCELERATED;
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S && WindowSetup.flag == clickableFlag) {
-                params.alpha = MAXIMUM_OPACITY_ALLOWED_FOR_S_AND_HIGHER;
-            } else {
-                params.alpha = 1;
-            }
-            windowManager.updateViewLayout(flutterView, params);
-            result.success(true);
-        } else {
-            result.success(false);
-        }
-    }
-
-    private void resizeOverlay(int width, int height, MethodChannel.Result result) {
-        if (windowManager != null) {
-            WindowManager.LayoutParams params = (WindowManager.LayoutParams) flutterView.getLayoutParams();
-            params.width = (width == -1999 || width == -1) ? -1 : dpToPx(width);
-            params.height = (height != 1999 || height != -1) ? dpToPx(height) : height;
-            windowManager.updateViewLayout(flutterView, params);
-            result.success(true);
-        } else {
-            result.success(false);
-        }
-    }
-
 
     @Override
     public void onCreate() {
@@ -280,93 +225,4 @@ public class OverlayService extends Service implements View.OnTouchListener {
     private int getDrawableResourceId(String resType, String name) {
         return getApplicationContext().getResources().getIdentifier(String.format("ic_%s", name), resType, getApplicationContext().getPackageName());
     }
-
-    private int dpToPx(int dp) {
-        return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP,
-                Float.parseFloat(dp + ""), mResources.getDisplayMetrics());
-    }
-
-    @Override
-    public boolean onTouch(View view, MotionEvent event) {
-        if (windowManager != null && WindowSetup.enableDrag) {
-            WindowManager.LayoutParams params = (WindowManager.LayoutParams) flutterView.getLayoutParams();
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN:
-                    dragging = false;
-                    lastX = event.getRawX();
-                    lastY = event.getRawY();
-                    break;
-                case MotionEvent.ACTION_MOVE:
-                    float dx = event.getRawX() - lastX;
-                    float dy = event.getRawY() - lastY;
-                    if (!dragging && dx * dx + dy * dy < 25) {
-                        return false;
-                    }
-                    lastX = event.getRawX();
-                    lastY = event.getRawY();
-                    int xx = params.x + (int) dx;
-                    int yy = params.y + (int) dy;
-                    params.x = xx;
-                    params.y = yy;
-                    windowManager.updateViewLayout(flutterView, params);
-                    dragging = true;
-                    break;
-                case MotionEvent.ACTION_UP:
-                case MotionEvent.ACTION_CANCEL:
-                    lastYPosition = params.y;
-                    if (WindowSetup.positionGravity != "none") {
-                        windowManager.updateViewLayout(flutterView, params);
-                        mTrayTimerTask = new TrayAnimationTimerTask();
-                        mTrayAnimationTimer = new Timer();
-                        mTrayAnimationTimer.schedule(mTrayTimerTask, 0, 25);
-                    }
-                    return false;
-                default:
-                    return false;
-            }
-            return false;
-        }
-        return false;
-    }
-
-    private class TrayAnimationTimerTask extends TimerTask {
-        int mDestX;
-        int mDestY;
-        WindowManager.LayoutParams params = (WindowManager.LayoutParams) flutterView.getLayoutParams();
-
-        public TrayAnimationTimerTask() {
-            super();
-            mDestY = lastYPosition;
-            switch (WindowSetup.positionGravity) {
-                case "auto":
-                    mDestX = (params.x + (flutterView.getWidth() / 2)) <= szWindow.x / 2 ? 0 : szWindow.x - flutterView.getWidth();
-                    return;
-                case "left":
-                    mDestX = 0;
-                    return;
-                case "right":
-                    mDestX = szWindow.x - flutterView.getWidth();
-                    return;
-                default:
-                    mDestX = params.x;
-                    mDestY = params.y;
-                    return;
-            }
-        }
-
-        @Override
-        public void run() {
-            mAnimationHandler.post(() -> {
-                params.x = (2 * (params.x - mDestX)) / 3 + mDestX;
-                params.y = (2 * (params.y - mDestY)) / 3 + mDestY;
-                windowManager.updateViewLayout(flutterView, params);
-                if (Math.abs(params.x - mDestX) < 2 && Math.abs(params.y - mDestY) < 2) {
-                    TrayAnimationTimerTask.this.cancel();
-                    mTrayAnimationTimer.cancel();
-                }
-            });
-        }
-    }
-
-
 }
